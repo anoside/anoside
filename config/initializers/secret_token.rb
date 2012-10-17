bd0b33fb1d2aa5dcfc03b4f31892daed237ebe65
Koji Shinba <bojovs@gmail.com>
2012-10-18 00:20:54 +0900

    $ rails new anoside --skip-bundle -d mysql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anoside::Application.config.secret_token = 'f460f4b8eb84cbe206ac36331833409deb92646bdcfcd850778cdacc3647e6db22b579d9e72190515e46ea5c3a6b88b0f0b26223d79b5ec52c7ba99aa5c30f2b'
