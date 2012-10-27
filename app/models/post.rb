f49e828a8375463df2d8023502e00db428b1f799
Koji Shinba <bojovs@gmail.com>
2012-10-27 18:24:14 +0900

    fix comments rendering

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :title
 
   has_many :comments
+  has_many :recent_comments, class_name: 'Comment', order: 'created_at DESC', limit: 5
 
   validates :body, length: { maximum: 500 }, presence: true
 
