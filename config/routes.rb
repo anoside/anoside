bcf4b17964e1c19fa3fbd19863759400abd67017
Koji Shinba <bojovs@gmail.com>
2012-11-04 01:45:03 +0900

    restrict resource actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Anoside::Application.routes.draw do
   devise_for :users,
     path_names: { sign_in: 'signin', sign_out: 'signout', sign_up: 'signup' }
 
-  resources :posts do
-    resources :comments
+  resources :posts, only: [:create, :destroy, :index, :update] do
+    resources :comments, only: [:create]
   end
 
   root to: 'posts#index'
