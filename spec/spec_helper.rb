099c05753230a5d6b25fccd20b18a41a6a641f71
Koji Shinba <bojovs@gmail.com>
2013-02-24 01:58:58 +0900

    Run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,4 +41,10 @@ RSpec.configure do |config|
   # https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
   config.include Warden::Test::Helpers
   Warden.test_mode!
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
 end
