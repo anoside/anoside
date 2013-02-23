342ec173b42244a971d8b6126324ec093813a48b
Koji Shinba <bojovs@gmail.com>
2013-02-24 01:57:04 +0900

    update factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,11 +2,15 @@ FactoryGirl.define do
   factory :user do
     sequence(:username) { |n| "username_#{n}" }
     password 'password'
-    guest false
+    accept_language_id 1
   end
 
   factory :post do
     sequence(:title) { |n| "title-#{n}" }
     sequence(:body) { |n| "body-#{n}" }
+  end
+
+  factory :language do
+    code 'en'
   end
 end
