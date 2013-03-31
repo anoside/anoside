26ae640299775f32e73325fd1c106c20ddefc0b5
Koji Shinba <bojovs@gmail.com>
2013-03-31 12:28:27 +0900

    application.js.coffee -> application.js

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require components/jquery/jquery.min
+//= require jquery_ujs
+//= require components/angular/angular
+//= require angular-sanitize.min
+//= require components/underscore/underscore-min
+
+//= require base
+
+//= require comments
+//= require posts
+//= require tags
