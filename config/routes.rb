8a2a9467e2ab24d7b6875011a517bc7c3c6fc69e
Koji Shimba <bojovs@gmail.com>
2013-08-24 22:10:57 +0900

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Anoside::Application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' },
-             spath_names: { sign_in: 'signin', sign_out: 'signout', sign_up: 'signup' }
+             path_names: { sign_in: 'signin', sign_out: 'signout', sign_up: 'signup' }
   root to: 'pages#index'
 end
