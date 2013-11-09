072f80450fa9001cabf0e4e2251da2f564f8be0b
Koji Shimba <bojovs@gmail.com>
2013-11-09 23:49:52 +0900

    settings.email allows nil

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,5 +2,5 @@ class Setting < ActiveRecord::Base
   has_many :languages_settings
   has_many :languages, through: :languages_settings
 
-  validates :email, uniqueness: true, email: true
+  validates :email, uniqueness: true, email: true, allow_nil: true
 end
