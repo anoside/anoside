cd01ba06947b9a5b8c2570ea02b9802275703d36
Koji Shimba <bojovs@gmail.com>
2013-11-17 17:53:41 +0900

    Add spec that tests tag addition feature

---
+++ spec/factories/post.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    language
+    title 'Hello World'
+    body 'Hello World. I am bojovs.'
+  end
+end
---
+++ spec/features/tags_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'Tagging', type: :feature, js: true do
+  before do
+    language = create(:language)
+    user = create(:user, accept_language_id: language.id)
+    post = create(:post, language: language)
+    user.make_viewpoint(post, original: true)
+
+    login_as(user, scope: :user)
+  end
+
+  it 'User can add tags to posts' do
+    visit root_path
+
+    find('i.icon-tag').click
+    fill_in 'post[tag_list]', with: 'tag1, tag2'
+    click_button 'Save'
+
+    expect(page).to have_link('tag1', href: '/tags/tag1')
+    expect(page).to have_link('tag2', href: '/tags/tag2')
+  end
+end
