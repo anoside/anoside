36015936ec06beaff9541e83501f21e7adf41c9b
Koji Shimba <bojovs@gmail.com>
2013-12-22 00:22:00 +0900

    ステージングの設定ファイルにdefault_url_optionsを追加

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,6 +82,8 @@ Anoside::Application.configure do
   # https://shellycloud.com/blog/2013/10/how-to-integrate-angularjs-with-rails-4
   config.assets.js_compressor = Uglifier.new(mangle: false)
 
+  config.action_mailer.default_url_options = { protocol: 'http://', host: Settings.host }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        Settings.smtp_host,
