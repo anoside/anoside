b6773e40753b7f7d2f76bd949c8d8bab18e686e5
Koji Shimba <bojovs@gmail.com>
2013-12-22 22:45:43 +0900

    デプロイ先のサーバ情報を修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :deploy_to, '/var/www/anoside'
 set :rails_env, 'production'
 set :user,      'root' # SSH
 
-server '192.168.33.10', :app, :web, :db, primary: true
+server '192.168.11.13', :app, :web, :db, primary: true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-set :branch,    'staging'
-set :deploy_to, '/var/www/anoside'
+set :branch,    'master'
+set :deploy_to, '/var/apps/anoside'
 set :rails_env, 'staging'
 set :user,      'root' # SSH
 
-server '192.168.33.10', :app, :web, :db, primary: true
+server '192.168.11.14', :app, :web, :db, primary: true
